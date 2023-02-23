683d0667b0a36f87b33e584eaae02ec9145e613b
Herman100 <hermankwamebour30@gmail.com>
2023-02-23 15:42:04 -0800

    edit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,11 @@
  * Return: true or false
  */
 int _isupper(int c);
+
+/**
+ * _isdigit - checks if it is digit
+ * @c: character to check
+ *
+ * Return: returns boolean
+ */
+int _isdigit(int c);
