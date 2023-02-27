4acb7dbf1a1c68006debc5aee7707a3c698c8cfc
Herman100 <hermankwamebour30@gmail.com>
2023-02-27 13:46:30 +0000

    error corrected for typo

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,24 +1,19 @@
 #include "main.h"
 
 /**
- * print_rev - prints given string in reverse
+ * _puts - print a given string to screen
  *
- * @s: passed pointer argument for string
+ * @str: passed pointer argument for string
  * Return: void
  */
-void print_rev(char *s)
+void _puts(char *str)
 {
 	int len = 0;
 
-	while (*(s + len) != '\0')
+	while (*(str + len) != '\0')
 	{
+		_putchar(*(str + len));
 		len++;
-	}
-	len--;
-	while (len >= 0)
-	{
-		_putchar(*(s + len));
-		len--;
 	}
 	_putchar('\n');
 }
