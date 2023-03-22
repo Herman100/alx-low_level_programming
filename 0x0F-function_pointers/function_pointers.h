73f2c3e638612f28403fd327b15ff0f448463951
Herman100 <hermankwamebour30@gmail.com>
2023-03-22 12:28:05 +0000

    header update

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,24 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+
+
+/**
+ * print_name - prints the name to std
+ * @name: input name
+ * @f: a function pointer
+ * Return: 0
+ */
+
+void print_name(char *name, void (*f)(char *));
+
+
+
+
+
+
+
+
+
+#endif
