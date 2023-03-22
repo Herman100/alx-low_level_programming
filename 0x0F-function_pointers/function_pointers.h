5ec5d8375f5e24894470b07151e03ab5cc932192
Herman100 <hermankwamebour30@gmail.com>
2023-03-22 12:33:19 +0000

    header updated

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -13,9 +13,15 @@
 
 void print_name(char *name, void (*f)(char *));
 
+/**
+ * array_iterator - iterates through an array of pointers
+ * @array: defines the function array
+ * @action: fuction pointer
+ * @size: determines size_t
+ * Return: 0
+ */
 
-
-
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 
 
