2dbee0afa1b60165eed82676c19dd8187fa95bf6
Herman100 <hermankwamebour30@gmail.com>
2023-03-30 21:02:28 +0000

    code updated

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,20 @@
-; File: hello.asm
-; Assembly program to print "Hello, Holberton" using printf
+section .data
+    hello db 'Hello, Holberton', 10, 0 ; 10 is the newline character, 0 is the null terminator
 
-        global  main
-        extern  printf
+section .text
+    global main
+    extern printf
 
-        section .data
-msg:    db      'Hello, Holberton', 0
-
-        section .text
 main:
-        ; Prepare arguments for printf
-        mov     rdi, msg        ; Set first argument to address of message string
-        xor     rax, rax        ; Clear rax register (number of vector registers used)
+    push rbp
+    mov rbp, rsp
 
-        ; Call printf
-        call    printf
+    lea rdi, [hello] ; Load the address of the hello string into rdi
+    xor eax, eax ; Clear eax to indicate there are no floating-point arguments
+    call printf ; Call printf with the hello string as the argument
 
-        ; Exit program
-        xor     rax, rax        ; Set return value to 0
-        ret                     ; Return from main function
+    mov rsp, rbp
+    pop rbp
+    xor eax, eax
+    ret
+
