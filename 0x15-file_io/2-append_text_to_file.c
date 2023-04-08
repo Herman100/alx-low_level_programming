d27a7115911619fb223ef3428af303bc45a67bf8
Herman100 <hermankwamebour30@gmail.com>
2023-04-08 09:13:25 +0000

    betty style added

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,6 +4,13 @@
 #include <unistd.h>
 #include <string.h>
 
+/**
+ * append_text_to_file - appends text to the end of a file
+ * @filename: name of the file to append to
+ * @text_content: content to append to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
@@ -11,12 +18,16 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
+
 	if (text_content)
 		len = write(fd, text_content, strlen(text_content));
+
 	close(fd);
+
 	return (len == -1 ? -1 : 1);
 }
 
